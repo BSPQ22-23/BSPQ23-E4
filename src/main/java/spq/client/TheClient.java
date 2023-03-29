c2250067386b03598cdd09ad1fa0c85d795755d1
ibarra18 <javier.ibarra@opendeusto.es>
2023-03-29 16:48:27 +0200

    Server and register with jersey and jetty

---
+++ src/main/java/spq/client/TheClient.java
@@ -0,0 +1,105 @@
+/**********************************************************************
+Copyright (c) 2003 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package spq.client;
+
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Extent;
+import javax.jdo.Query;
+import javax.jdo.JDOHelper;
+import javax.jdo.Transaction;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import spq.serialitazion.UserData;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * Controlling application for the DataNucleus Tutorial using JDO.
+ * Relies on the user defining a file "datanucleus.properties" to be in the CLASSPATH
+ * and to include the JDO properties for the DataNucleus PersistenceManager.
+ * @author      Diego Lopez-de-Ipina 
+ * @version     1.0                                    
+ * @since       2012-02-01        
+ */
+public class TheClient
+{
+	private static final String USER = "clienteparapruebas";
+	private static final String PASSWORD = "clienteparapruebas";
+    
+	protected static final Logger logger = LogManager.getLogger();
+    private Client client;
+    private WebTarget webTarget;
+	
+    
+    
+    public TheClient(String hostname, String port) {
+		client = ClientBuilder.newClient();
+		webTarget = client.target(String.format("http://%s:%s/rest/resource", hostname, port));
+	}
+    
+    public void registerUser(String name, String password,double purse, int type) {
+    	WebTarget registerUserWebTarget = webTarget.path("register");
+    	Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
+		
+    	UserData userData =new UserData();
+    	userData.setName(name);
+    	userData.setPassword(password);
+    	userData.setPurse(purse);
+    	userData.setType(type);
+    	Response response = invocationBuilder.post(Entity.entity(userData, MediaType.APPLICATION_JSON));
+    	if (response.getStatus() != Status.OK.getStatusCode()) {
+			logger.error("Error connecting with the server. Code: {}", response.getStatus());
+		} else {
+			logger.info("User correctly registered");
+		}
+    	
+    
+    }
+    public static void main(String[] args) {
+		if (args.length != 2) {
+			logger.info("Use: java Client.Client [host] [port]");
+			System.exit(0);
+		}
+
+		String hostname = args[0];
+		String port = args[1];
+		
+		TheClient newclient= new TheClient(hostname, port);
+		newclient.registerUser(USER, PASSWORD, 23, 0);
+		
+		
+		
+    }
+    
+   
+    
+}
--- src/main/java/spq/server/Server.java
+++ src/main/java/spq/server/Server.java
@@ -1,5 +1,91 @@
 package spq.server;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.JDOHelper;
+import javax.jdo.Transaction;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.logging.log4j.Logger;
+
+import spq.jdo.User;
+import spq.serialitazion.UserData;
+
+import org.apache.logging.log4j.LogManager;
+@Path("/resource")
+@Produces(MediaType.APPLICATION_JSON)
 
 public class Server {
+	
+	protected static final Logger logger = LogManager.getLogger();
+	private int cont = 0;
+	private PersistenceManager pm=null;
+	private Transaction tx=null;
 
+	public Server() {
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		this.pm = pmf.getPersistenceManager();
+		this.tx = pm.currentTransaction();
+	}
+	
+	
+	@POST
+	@Path("/register")
+	public Response registerUser(UserData userData) {
+		try {
+			tx.begin();
+			logger.info("Checking whether the user already exits or not: '{}'",userData.getName());
+			User user=null;
+			try {
+				user= pm.getObjectById(User.class,userData.getName());
+			}catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				logger.info("Exception launched: {}", jonfe.getMessage());
+			}
+			logger.info("User: {}",user);
+			if (user!=null) {
+				logger.info("Setting password user: {}", user);
+				user.setPassword(userData.getPassword());
+				logger.info("Password set user: {}", user);
+				
+				logger.info("Setting purse user: {}", user);
+				user.setPurse(userData.getPurse());
+				logger.info("Purse set user:{}", user);
+				
+				logger.info("Setting type user: {}", user);
+				user.setType(0);
+				logger.info("Type set user:{}", user);
+				
+			}else {
+				logger.info("Creating user: {}", user);
+				user= new User(userData.getName(),userData.getPassword(),userData.getPurse(),0);
+				pm.makePersistent(user);
+				logger.info("User created: {}", user);
+			}
+			tx.commit();
+			return Response.ok().build();
+
+			
+		}finally {
+			 if (tx.isActive())
+	            {
+	                tx.rollback();
+	            }
+		}
+	}
+	@GET
+	@Path("/hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sayHello() {
+		return Response.ok("Hello world!").build();
+	}
+	
+	
 }
