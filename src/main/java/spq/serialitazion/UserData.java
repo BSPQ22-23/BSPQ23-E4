fbe47ed7303ba823fe4e3765eb9f99249be01f87
ibarra18 <javier.ibarra@opendeusto.es>
2023-03-29 14:40:28 +0200

    Very Basic User changes ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡

--- src/main/java/spq/serialitazion/UserData.java
+++ src/main/java/spq/serialitazion/UserData.java
@@ -1,5 +1,40 @@
 package spq.serialitazion;
 
 public class UserData {
-
+	 private String name;
+	 private String password;
+	 private double purse;
+	 private int type;
+	 public UserData() {
+	        // required by serialization
+	    }
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public double getPurse() {
+		return purse;
+	}
+	public void setPurse(double purse) {
+		this.purse = purse;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "UserData [name=" + name + ", password=" + password + "]";
+	}
+	 
 }
