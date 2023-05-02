25cfcfa6de368381431f7cdc4128c01c7b5b03e8
ibarra18 <javier.ibarra@opendeusto.es>
2023-05-02 15:35:20 +0200

    adding the test we did in class

--- src/test/java/JUnitPerfT/Tests.java
+++ src/test/java/JUnitPerfT/Tests.java
@@ -1,14 +1,22 @@
 package JUnitPerfT;
+
 import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
 import spq.client.TheClient;
+import spq.jdo.Product;
+import spq.serialitazion.ProductData;
+import spq.server.Server;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
+
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.slf4j.LoggerFactory;
 
 import com.github.noconnor.junitperf.JUnitPerfRule;
@@ -16,43 +24,44 @@ import com.github.noconnor.junitperf.JUnitPerfTest;
 import com.github.noconnor.junitperf.JUnitPerfTestRequirement;
 import com.github.noconnor.junitperf.reporting.providers.HtmlReportGenerator;
 
-import spq.serialitazion.ProductData;
-
-
 public class Tests {
-	  private Client client;
-	    private WebTarget webTarget;
-	    String hostname="localhost";
-		String  port = "8080";
-		TheClient newclient= new TheClient(hostname, port);
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Tests.class);
+	private String hostname = "localhost";
+	private String port = "8080";
+	private TheClient newclient;
+	ProductData p = new ProductData("Movil", 234, true);
+
+	@Before
+	public void setUp() {
+		newclient = new TheClient(hostname, port);
+		ProductData p = new ProductData("Movil", 234, true);
+	}
+
 	@Rule
-	 public JUnitPerfRule rule = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.html"));
-	
-	
-	
-	    @Rule
-	    public JUnitPerfRule perfRule = new JUnitPerfRule();
+	public JUnitPerfRule rule = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.html"));
+	@Rule
+	public JUnitPerfRule rule1 = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.csv"));
+	@Mock
+	Server s;
 
-	    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Tests.class);
+	@Test
+	@JUnitPerfTest(durationMs = 3000, threads = 30, warmUpMs = 500, maxExecutionsPerSecond = 500)
+	public void testLoginUser() throws Exception {
 
-	    @Test
-	    @JUnitPerfTest(durationMs = 3000, threads = 30, warmUpMs = 500, maxExecutionsPerSecond = 500)
-	    public void testLoginUser() {
-	       
-	        String name = "admin";
-	        String password = "admin";
-	        
-			newclient.loginUser(name, password);
-	        
-	        assertNotNull(newclient.loginUser(name, password));
-	    }
-	    @Test
-	    @JUnitPerfTest(threads = 10, durationMs = 5000)
-	    @JUnitPerfTestRequirement(executionsPerSec = 200, allowedErrorPercentage = (float) 0.05)
-	    public void testGetAvailableProducts() {
-	    	 List<ProductData> products = newclient.getAvailableProducts();
-	         assertNotNull(products);
-	    }
-	    }
-	    
-	  
+		String name = "admin";
+		String password = "admin";
+		assertNotNull(newclient.loginUser(name, password));
+	}
+
+	@Test
+	@JUnitPerfTest(durationMs = 3000, threads = 30, warmUpMs = 500, maxExecutionsPerSecond = 500)
+	public void TestaddProduct() {
+		assertNotNull(newclient.addProduct(p.getName(), p.getPrice(), true));
+	}
+	@Test
+    @JUnitPerfTest(threads = 10, durationMs = 5000)
+    public void testGetAvailableProducts() {
+    	 List<ProductData> products = newclient.getAvailableProducts();
+         assertNotNull(products);
+    }
+}
