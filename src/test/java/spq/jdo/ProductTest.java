d45e0275e26e27b599b800d9622ea099734b3b03
Mikel Muñoz <mikel.maranguren@opendeusto.es>
2023-05-16 11:44:07 +0200

    Documentation for jdo in test

--- src/test/java/spq/jdo/ProductTest.java
+++ src/test/java/spq/jdo/ProductTest.java
@@ -5,37 +5,58 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-
+/**
+ * This class contains unit tests for the Product class.
+ */
 public class ProductTest {
 	private Product p;
     
+	/**
+     * Set up the test environment by initializing a Product object with test data.
+     * This method is executed before each test case.
+     */
     @Before
     public void setUp() {
         p = new Product("PC-Dell", 10.5,false);
     }
 
+    /**
+     * Test case to verify the correctness of the getName() method.
+     */
     @Test
     public void testGetName() {
         assertEquals("PC-Dell", p.getName());
     }
 
+    /**
+     * Test case to verify the correctness of the setName() method.
+     */
     @Test
     public void testSetName() {
         p.setName("PC-MSI");
         assertEquals("PC-MSI", p.getName());
     }
 
+    /**
+     * Test case to verify the correctness of the getPrice() method.
+     */
     @Test
     public void testGetPrice() {
         assertEquals(10.5, p.getPrice(), 0.0);
     }
 
+    /**
+     * Test case to verify the correctness of the setPrice() method.
+     */
     @Test
     public void testSetPrice() {
         p.setPrice(20.5);
         assertEquals(20.5, p.getPrice(), 0.0);
     }
 
+    /**
+     * Test case to verify the correctness of the toString() method.
+     */
     @Test
     public void testToString() {
         assertEquals("Product [name= PC-Dell, price= 10.5, available= false]", p.toString());
--- src/test/java/spq/jdo/UserTest.java
+++ src/test/java/spq/jdo/UserTest.java
@@ -12,37 +12,59 @@ import org.junit.Test;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
+/**
+ * This class contains unit tests for the User class.
+ */
 public class UserTest {
     
     private static ZonedDateTime timestamp = ZonedDateTime.of(2023, 03, 23, 19, 15, 22, 0, ZoneId.of("Europe/Madrid"));
     private User user;
 
+    /**
+     * Set up the test environment by initializing a User object with test data.
+     * This method is executed before each test case.
+     */
     @Before
     public void setUp() {
         user = new User("test-login", "passwd", 12.34, 0);
     }
 
+    /**
+     * Test case to verify the correctness of the getName() method.
+     */
     @Test
     public void testGetName() {
         assertEquals("test-login", user.getName());
     }
 
+    /**
+     * Test case to verify the correctness of the getPassword() method.
+     */
     @Test
     public void testGetPassword() {
         assertEquals("passwd", user.getPassword());
     }
 
+    /**
+     * Test case to verify the correctness of the setPassword() method.
+     */
     @Test
     public void testSetPassword() {
         user.setPassword("newpasswd");
         assertEquals("newpasswd", user.getPassword());
     }
 
+    /**
+     * Test case to verify the correctness of the getPurse() method.
+     */
     @Test
     public void testGetPurse() {
     	assertEquals(12.34, user.getPurse(), 0.01);
     }
     
+    /**
+     * Test case to verify the correctness of the setPurse() method.
+     */
     @Test
     public void testSetPurse() {
     	double p = user.getPurse() + 12.00;
@@ -50,13 +72,18 @@ public class UserTest {
         assertEquals(p, user.getPurse() , 0.01);
     }
     
+    /**
+     * Test case to verify the correctness of the getType() method.
+     */
     @Test
     public void testGetType() {
         int type = 0;
         assertEquals(type, user.getType());
     }
 
-
+    /**
+     * Test case to verify the correctness of the toString() method.
+     */
     @Test
     public void testToString() {
         String expectedString = "Name = test-login, Password = passwd, Your money = 12.34";
