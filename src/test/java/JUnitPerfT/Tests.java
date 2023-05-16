2e54ed0c794d27a8430d433d14011268d72d7b0b
markelmorales15 <markeluribe@opendeusto.es>
2023-05-16 12:17:22 +0200

    Documentation about performance test for the doxygen

--- src/test/java/JUnitPerfT/Tests.java
+++ src/test/java/JUnitPerfT/Tests.java
@@ -24,44 +24,93 @@ import com.github.noconnor.junitperf.JUnitPerfTest;
 import com.github.noconnor.junitperf.JUnitPerfTestRequirement;
 import com.github.noconnor.junitperf.reporting.providers.HtmlReportGenerator;
 
+/**
+ * The `Tests` class contains JUnit tests to verify the functionality of the `TheClient` class.
+ * 
+ * @author [Author Name]
+ * @version [Version Number]
+ */
 public class Tests {
-	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Tests.class);
-	private String hostname = "localhost";
-	private String port = "8080";
-	private TheClient newclient;
-	ProductData p = new ProductData("Movil", 234, true);
+    
+    /** The logger for this class. */
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Tests.class);
+    
+    /** The hostname of the server to connect to. */
+    private String hostname = "localhost";
+    
+    /** The port number of the server to connect to. */
+    private String port = "8080";
+    
+    /** The client to use for testing. */
+    private TheClient newclient;
+    
+    /** A product data object to use for testing. */
+    ProductData p = new ProductData("Movil", 234, true);
 
-	@Before
-	public void setUp() {
-		newclient = new TheClient(hostname, port);
-		ProductData p = new ProductData("Movil", 234, true);
-	}
+    /**
+     * Set up the test environment by creating a new client and product data object.
+     */
+    @Before
+    public void setUp() {
+        newclient = new TheClient(hostname, port);
+        ProductData p = new ProductData("Movil", 234, true);
+    }
 
-	@Rule
-	public JUnitPerfRule rule = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.html"));
-	@Rule
-	public JUnitPerfRule rule1 = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.csv"));
-	@Mock
-	Server s;
+    /** 
+     * A rule that generates a JUnitPerf report in HTML format.
+     */
+    @Rule
+    public JUnitPerfRule rule = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.html"));
+    
+    /** 
+     * A rule that generates a JUnitPerf report in CSV format.
+     */
+    @Rule
+    public JUnitPerfRule rule1 = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.csv"));
+    
+    /** 
+     * A mocked server to use in testing.
+     */
+    @Mock
+    Server s;
+    
+    /**
+     * Test the `loginUser` method of the `TheClient` class.
+     * 
+     * @throws Exception if an error occurs while running the test
+     */
+    @Test
+    @JUnitPerfTest(durationMs = 3000, threads = 30, warmUpMs = 500, maxExecutionsPerSecond = 500)
+    public void testLoginUser() throws Exception {
 
-	@Test
-	@JUnitPerfTest(durationMs = 3000, threads = 30, warmUpMs = 500, maxExecutionsPerSecond = 500)
-	public void testLoginUser() throws Exception {
+        // Set up test data
+        String name = "admin";
+        String password = "admin";
+        
+        // Perform the test
+        assertNotNull(newclient.loginUser(name, password));
+    }
 
-		String name = "admin";
-		String password = "admin";
-		assertNotNull(newclient.loginUser(name, password));
-	}
-
-	@Test
-	@JUnitPerfTest(durationMs = 3000, threads = 30, warmUpMs = 500, maxExecutionsPerSecond = 500)
-	public void TestaddProduct() {
-		assertNotNull(newclient.addProduct(p.getName(), p.getPrice(), true));
-	}
-	@Test
+    /**
+     * Test the `addProduct` method of the `TheClient` class.
+     */
+    @Test
+    @JUnitPerfTest(durationMs = 3000, threads = 30, warmUpMs = 500, maxExecutionsPerSecond = 500)
+    public void TestaddProduct() {
+        
+        // Perform the test
+        assertNotNull(newclient.addProduct(p.getName(), p.getPrice(), true));
+    }
+    
+    /**
+     * Test the `getAvailableProducts` method of the `TheClient` class.
+     */
+    @Test
     @JUnitPerfTest(threads = 10, durationMs = 5000)
     public void testGetAvailableProducts() {
-    	 List<ProductData> products = newclient.getAvailableProducts();
-         assertNotNull(products);
+        
+        // Perform the test
+        List<ProductData> products = newclient.getAvailableProducts();
+        assertNotNull(products);
     }
 }
